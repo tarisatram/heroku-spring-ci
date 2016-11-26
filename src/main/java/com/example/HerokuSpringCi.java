d563745bdab8094435f384dc830638ee65dda85c
Taris Atram <taris.atram@outlook.com>
2016-11-25 23:47:49 -0500

    Added initial response mapping.

--- src/main/java/com/example/HerokuSpringCi.java
+++ src/main/java/com/example/HerokuSpringCi.java
@@ -2,9 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @SpringBootApplication
 public class HerokuSpringCi {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuSpringCi.class, args);
